2b7e6159242f8b466b396894fad63b181fed4d38
Jerin Thomas <jerinthomas1404@gmail.com>
2022-09-22 23:06:55 +0530

    Updates

---
+++ precache-manifest.e9380c8bc773c9363d5caeb42fca8fc0.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f78716f1995a571d1a716ab92e7e2b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15b6e1d2d7cc8e3670b6",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "68fc5457aef6dae0ec42",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "d5336d08a6107d8f8616",
+    "url": "/static/css/main.de12e52e.chunk.css"
+  },
+  {
+    "revision": "6228d7c75decc98b3f05",
+    "url": "/static/js/2.e7e835bd.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.e7e835bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b6e1d2d7cc8e3670b6",
+    "url": "/static/js/3.471574ff.chunk.js"
+  },
+  {
+    "revision": "68fc5457aef6dae0ec42",
+    "url": "/static/js/4.58e6e918.chunk.js"
+  },
+  {
+    "revision": "d5336d08a6107d8f8616",
+    "url": "/static/js/main.5a5d3583.chunk.js"
+  },
+  {
+    "revision": "ab256124bdf732c07672",
+    "url": "/static/js/runtime-main.c8bfb87b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c0457c503fda66b4e8b391a7d6fb5c8d",
+    "url": "/static/media/certification.c0457c50.gif"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "2ec8944855e7dce41b220f4f87bf9a16",
+    "url": "/static/media/dps.2ec89448.png"
+  },
+  {
+    "revision": "6d92a01caee6638a042339e4daf1d19c",
+    "url": "/static/media/driveralertness.6d92a01c.jpg"
+  },
+  {
+    "revision": "03a4a5f034bf0bafa661fd8a8aabedc8",
+    "url": "/static/media/hackathon.03a4a5f0.gif"
+  },
+  {
+    "revision": "d1d492d379321ea45087f37d80206cdb",
+    "url": "/static/media/ivaidya.d1d492d3.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "47fd82141a51adb2ff9eb5ea5a5e60bc",
+    "url": "/static/media/nhps.47fd8214.png"
+  },
+  {
+    "revision": "bda6977e5ce05c0cc187fd3710dd8b46",
+    "url": "/static/media/research.bda6977e.gif"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "10e1caf8309543b19a84d62cccf935ce",
+    "url": "/static/media/tcs.10e1caf8.png"
+  },
+  {
+    "revision": "3b20b788369676a4dda11dbf308e41c9",
+    "url": "/static/media/tcs2.3b20b788.jpg"
+  },
+  {
+    "revision": "e9d96d85f50a591026e80ab8163acb1b",
+    "url": "/static/media/universityofmumbai.e9d96d85.png"
+  }
+]);
